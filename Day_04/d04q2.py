2e5171f24a6e992ba747f29fc1caeb2a6088b3db
PNelson3 <peteranelson3rd@gmail.com>
2022-12-04 00:42:31 -0500

    Day 4 solution

---
+++ Day_04/d04q2.py
@@ -0,0 +1,18 @@
+fileName = input('Input:\n')
+
+with open(fileName, 'r') as f:
+    pairs = f.readlines()
+
+total = 0
+
+for i in pairs:
+    left = i[:i.index(',')]
+    right = i[i.index(',') + 1:]
+    ll = int(left[:left.index('-')])
+    lh = int(left[left.index('-') + 1:])
+    rl = int(right[:right.index('-')])
+    rh = int(right[right.index('-') + 1:])
+    if ((rh <= lh) and (rh >= ll)) or ((lh <= rh) and (lh >= rl)):
+        total += 1
+
+print(total)
